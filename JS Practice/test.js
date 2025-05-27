7308db433393587e6b3321d94a9d64779fe30303
Afridi <roshidulalom2017@gmail.com>
2025-05-27 11:27:40 +0600

    practice1

--- JS Practice/test.js
+++ JS Practice/test.js
@@ -1,4 +1,11 @@
 function change(){
-    document.getElementById("demo").innerHTML=window.alert(5+2);
+    let x=5,y=10;
+    let z=x+y;
+    {
+        let z=x-y;
+        document.getElementById("demo1").innerHTML=z;
+    }
+    let $name="afridi alom pranto"
+    document.getElementById("demo").innerHTML=z+" "+$name + " " +z;
    
 }
